ccd9953dc28a41f609c91f653a245bd8c31d7688
Lucachak <lucas.lucachak9@gmail.com>
2025-04-14 13:23:01 +0200

    add some admin stuff

--- Home/admin.py
+++ Home/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import *
 # Register your models here.
 
-admin.site.register(UsersCount)
+
+@admin.register(UsersCount)
+class UserCount(ModelAdmin):
+    pass
